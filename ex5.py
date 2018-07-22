37b3622d577378f16e9d5299e23b7a2cc1752aca
Daniel Rouse-Rudd <drouserudd55@gmail.com>
2018-07-22 19:03:48 -0400

    Add files via upload

---
+++ ex5.py
@@ -0,0 +1,17 @@
+my_name = "Daniel A. Rouse-Rudd"
+my_age = 22
+my_height = 70 #inches
+my_weight = 220 #pounds
+my_eyes = "Brown"
+my_teeth = "White"
+my_hair = "Brown"
+
+print(f"Let's talk about {my_name}")
+print(f"He's {my_height} inches tall")
+print(f"He's {my_weight} pounds heavy.")
+print(f"He's got {my_eyes} eyes, and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} as he doesn't drink coffee. His soda addiction is cathing up to him though")
+
+total = my_age + my_height + my_weight
+
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
